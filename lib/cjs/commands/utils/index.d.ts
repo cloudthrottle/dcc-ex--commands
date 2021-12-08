a5dbbbca60de4ee2704ceddfb3d7365338be2737
David <davie668@gmail.com>
2021-12-08 21:40:06 +0000

    Removes lib folder

--- lib/cjs/commands/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { makeCommand } from "./makeCommand";
-import { parseAddress } from "./parseAddress";
-export { makeCommand, parseAddress };
--- lib/cjs/commands/utils/makeCommand.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const makeCommand: (instruction: string) => string;
