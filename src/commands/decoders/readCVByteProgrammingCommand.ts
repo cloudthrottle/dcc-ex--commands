9945eadb107b472fd3d5cbcda01f8c7e25ca8459
David <davie668@gmail.com>
2021-12-09 08:17:44 +0000

    Migrates readCVByteProgrammingCommand

---
+++ src/commands/decoders/readCVByteProgrammingCommand.ts
@@ -0,0 +1,24 @@
+import { makeCommand } from '../utils'
+
+export interface ReadCVByteProgrammingCommandParams { cv: number, callbackNum: number, callbackSub: number }
+
+const readProgrammingDecoderByteKey = 'R'
+
+/**
+ * READ CONFIGURATION VARIABLE BYTE FROM ENGINE DECODER ON PROGRAMMING TRACK
+ * https://dcc-ex.com/reference/software/command-reference.html#read-configuration-variable-byte-from-engine-decoder-on-programming-track
+ */
+export const readCVByteProgrammingCommand: (params: ReadCVByteProgrammingCommandParams) => string = ({
+  cv,
+  callbackNum,
+  callbackSub
+}) => {
+  const attributes = [
+    readProgrammingDecoderByteKey,
+    cv,
+    callbackNum,
+    callbackSub
+  ]
+  const str = attributes.join(' ')
+  return makeCommand(str)
+}
---
+++ tests/unit/decoders/readCVByteProgrammingCommand.test.ts
@@ -0,0 +1,15 @@
+import {readCVByteProgrammingCommand} from "../../../src";
+
+describe('readCVByteProgrammingCommand()', function () {
+    it('is valid', () => {
+        const options = {
+            cv: 14,
+            callbackNum: 1024,
+            callbackSub: 3
+        }
+        const sendString = '<R 14 1024 3>'
+
+        const command = readCVByteProgrammingCommand(options)
+        expect(command).toBe(sendString)
+    })
+})
