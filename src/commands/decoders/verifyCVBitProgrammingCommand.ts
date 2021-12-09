49d1d2cc4ec73e1fdb5d893b252dbfbdbf51c727
David <davie668@gmail.com>
2021-12-09 08:34:23 +0000

    Migrates verifyCVBitProgrammingCommand

---
+++ src/commands/decoders/verifyCVBitProgrammingCommand.ts
@@ -0,0 +1,21 @@
+import { makeCommand } from '../utils'
+import { Active } from '../../types'
+
+export interface VerifyCVBitProgrammingCommandParams { cv: number, bit: number, bitValue: Active }
+
+const verifyProgrammingDecoderBitKey = 'V'
+
+/**
+ * VERIFY CONFIGURATION VARIABLE BIT FROM ENGINE DECODER ON PROGRAMMING TRACK
+ * https://dcc-ex.com/reference/software/command-reference.html#verify-configuration-variable-bit-from-engine-decoder-on-programming-track
+ */
+export const verifyCVBitProgrammingCommand: (params: VerifyCVBitProgrammingCommandParams) => string = ({ cv, bit, bitValue }) => {
+  const attributes = [
+    verifyProgrammingDecoderBitKey,
+    cv,
+    bit,
+    bitValue
+  ]
+  const str = attributes.join(' ')
+  return makeCommand(str)
+}
---
+++ tests/unit/decoders/verifyCVBitProgrammingCommand.test.ts
@@ -0,0 +1,14 @@
+import {verifyCVBitProgrammingCommand, VerifyCVBitProgrammingCommandParams} from "../../../src";
+
+describe('verifyCVBitProgrammingCommand()', function () {
+    it('is valid', () => {
+        const options: VerifyCVBitProgrammingCommandParams = {
+            cv: 14,
+            bit: 5,
+            bitValue: 1
+        }
+        const sendString = '<V 14 5 1>'
+        const command = verifyCVBitProgrammingCommand(options)
+        expect(command).toBe(sendString)
+    })
+})
