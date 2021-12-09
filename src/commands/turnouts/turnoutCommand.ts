920283f9eecd6296d06df2710963b8bcfd840874
David <davie668@gmail.com>
2021-12-09 20:12:07 +0000

    Migrates turnoutCommand

---
+++ src/commands/turnouts/turnoutCommand.ts
@@ -0,0 +1,25 @@
+import { makeCommand } from '../utils'
+
+export enum TurnoutState {
+  CLOSED = 0,
+  THROWN = 1
+}
+export interface TurnoutCommandParams {
+  turnout: number
+  thrown: TurnoutState.CLOSED | TurnoutState.THROWN
+}
+
+const turnoutCommandKey = 'T'
+
+/**
+ * https://dcc-ex.com/reference/software/command-reference.html#controlling-a-defined-turnout
+ */
+export const turnoutCommand: (params: TurnoutCommandParams) => string = ({ turnout, thrown }) => {
+  const attributes = [
+    turnoutCommandKey,
+    turnout,
+    thrown.toString()
+  ]
+  const str = attributes.join(' ')
+  return makeCommand(str)
+}
---
+++ tests/unit/turnouts/turnoutCommand.test.ts
@@ -0,0 +1,14 @@
+import {turnoutCommand} from "../../../src";
+
+describe('turnoutCommand()', function () {
+    test('is valid', () => {
+        const options = {
+            turnout: 22,
+            thrown: 0
+        }
+        const sendString = '<T 22 0>'
+
+        const command = turnoutCommand(options)
+        expect(command).toEqual(sendString)
+    })
+})
