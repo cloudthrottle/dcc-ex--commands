5bf7ce14c84c35255201f4c4a2756411cc17d67b
David Young <davie668@gmail.com>
2024-01-05 15:18:29 +0000

    Updated Roster List and Item commands
    Changed from `<J>` to `<JR>`

--- tests/unit/commands/rosters/rosterCommand.test.ts
+++ tests/unit/commands/rosters/rosterCommand.test.ts
@@ -2,14 +2,14 @@ import { rosterCommand } from '../../../../src'
 
 describe('rosterCommand()', function () {
   it('is valid', function () {
-    const sendString = '<J>'
+    const sendString = '<JR>'
     const command = rosterCommand()
     expect(command).toEqual(sendString)
   })
 
   describe('for a specific Cab', () => {
     it('is valid', function () {
-      const sendString = '<J 70>'
+      const sendString = '<JR 70>'
       const command = rosterCommand({ cabId: 70 })
       expect(command).toEqual(sendString)
     })
